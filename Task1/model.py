eb3f3af9c8bfc4d3eba0842b1cd4c8b8206e7799
benjamin9555 <gallussb@student.ethz.ch>
2017-05-01 21:17:05 +0200

    first version of the entire training model

--- Task1/model.py
+++ Task1/model.py
@@ -1,15 +1,14 @@
 import tensorflow as tf
 import numpy as np
 
+import lstm
 from config import cfg
 
 
 def train_model(data, embeddings=None):
-    # 0. Cut the <eos> tags from x
-    # x = 
     
     # 1. Batching
-    inp = tf.placeholder(dtype=tf.float32, shape=[None,30,20000])
+    inp = tf.placeholder(dtype=tf.float32, shape=[None,cfg["sentence_length"], cfg["vocab_size"]])
     batch = tf.train.batch(
         tensors=inp,
         batch_size=cfg["batch_size"],
@@ -17,48 +16,75 @@ def train_model(data, embeddings=None):
         allow_smaller_final_batch=True,
     )
 
+    # from 3D tensor to 2D tensor, dim batch_Size x vocab_size
+    # returns a list of 2D tensors
+    batch_positionwise = tf.split(value=batch, num_or_size_splits = cfg["sentence_length"], axis=1)
+
+    # We can discard the last 2D tensor from batch_positionwise
+    # Cut the <eos> tags for the input data
+    #x = tf.slice(input_=one_input_position_batch, begin=[0,0], size=[-1, cfg["sentence_length"] - 1])
+    
     initializer = tf.contrib.layers.xavier_initializer()
     dtype = tf.float32
 
-    #2. Input Layer of 20000
-    # TODO: from batch to 1D vector of 20.000
-
-    one_word = None
     
     W_emb = []
     bias_emb = []
     fc_emb_layer = []
-    for i in range(29):
+
+    lstm_layer = []
+    lstm_out = []
+
+    W_out = []
+    bias_out = []
+    out_layer = []
+
+    y_hat = []
+    cost = []
+
+    for i in range(cfg["sentence_length"] - 1):
+        lstm_layer.append(LstmCell())
+
+    for i in range(cfg["sentence_length"] - 1):
 
         #3. Fully connected of 100
-        W_emb.append(tf.get_variable(dtype=dtype, shape=[cfg["embeddings_size"],cfg["vocab_size"]], initializer=initializer))
+        W_emb.append(tf.get_variable(dtype=dtype, shape=[cfg["vocab_size"], cfg["embeddings_size"]], initializer=initializer))
         bias_emb.append(tf.get_variable(dtype=dtype, shape=[cfg["embeddings_size"]], initializer=initializer))
 
-        fc_emb_layer.append(tf.nn.relu(W_emb * one_word + bias1))
 
-        #4. LSTM - Fred, dim 512
-        
-        lstm = None
+        fc_emb_layer.append(tf.nn.relu(tf.matmul(batch_positionwise[i], W_emb[i]) + bias_emb[i]))
+
+        #4. LSTM dim 512
+        if i == 0:
+            lstm_out.append(lstm_layer[i](X=fc_emb_layer[i], state=(tf.zeros(shape=[None,cfg["lstm_size"]], tf.zeros(shape=[None,cfg["lstm_size"]]))) ) )
+        else:
+            lstm_out.append(lstm_layer[i](X=fc_emb_layer[i], state=lstm_out[i-1]))
 
         #5. Linear FC output layer
         
         #Output layer
-        W_output = tf.get_variable(dtype=dtype, shape=[cfg["vocab_size"],cfg["lstm_size"]], initializer=initializer)
-        bias_output = tf.get_variable(dtype=dtype, shape=[cfg["vocab_size"]], initializer=initializer)
-        output_layer = W_output * lstm + bias_output
+        W_out.append( tf.get_variable(dtype=dtype, shape=[cfg["vocab_size"],cfg["lstm_size"]], initializer=initializer) )
+        bias_out.append( tf.get_variable(dtype=dtype, shape=[cfg["vocab_size"]], initializer=initializer) )
+        out_layer.append( W_out[i] * lstm_out[i] + bias_out[i] )
 
         #6. Softmax output + cat cross entropy loss
         
-        # TODO: Cut the <bos> tags from the labels
-        #y =  
-        
-        y_hat = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y, logits=output_layer)
+        # we ommit the 0-th word in each sentence (namely the <bos> tag).
+        # The labels start position 1
+        y_hat.append( tf.nn.sparse_softmax_cross_entropy_with_logits(labels=batch_positionwise[i+1], logits=out_layer[i]) )
 
-    #7. Somehow duplicate this and connect the hidden units
+        cost.append(tf.reduce_mean(y_hat[i]))   
+
+
+    concatenated_costs = tf.concat(values=cost, axis=0)
+    optimizer = tf.train.AdamOptimizer()
+    gvs = optimizer.compute_gradients(concatenated_costs)
+    capped_gvs = tf.clip_by_global_norm(t_list=[x[0] for x in gvs], clip_norm=10)
+    train_op = optimizer.apply_gradients(zip(capped_gvs, [x[1] for x in gvs]))
     
-    #8. standard Backprop, gradient optimizer
-    
- 
+    sess = tf.Session()
+    sess.run(fetches=train_op, feed_dict={inp:data})
+
  def test_model(data, params):
     # Forward Prop:
     # Same network, with trained parameters
